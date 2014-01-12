845c3265e41fd29b572aa5b87cb919585213ab59
Anton Petrov <givemeletter@gmail.com>
2014-01-13 00:42:04 +0100

    Make a basic User model

--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,7 @@ module SampleApp
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+
+    config.i18n.enforce_available_locales = false
   end
 end
