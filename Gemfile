784216781cac5c00d2bdf9bd8093af96d0094b93
Anton Petrov <givemeletter@gmail.com>
2014-01-15 00:48:25 +0100

    Finish user signup

--- Gemfile
+++ Gemfile
@@ -15,6 +15,7 @@ end
 group :test do
   gem 'selenium-webdriver'
   gem 'capybara'
+  gem 'factory_girl_rails'
 end
 
 # Use SCSS for stylesheets
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
+  resources :users
 
   root 'static_pages#home'
   get 'signup',  to: 'users#new'
